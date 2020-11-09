222e5a10963984d85744488d89aad088ba12d266
dariogustavolopez <dario.lopez@outlook.com.ar>
2020-11-09 00:00:20 -0300

    3 files modified, 1 added, 5 deleted

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from "../../models/cliente";
 
+import { MockClientesService } from "../../services/mock-clientes.service";
+import { clientesService } from "../../services/clientes.service";
+ 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+ Titulo= "Clientes";
+ Items: Cliente[]=[];
+ 
 
-  constructor() { }
-
+ 
+  constructor(
+    //private clienteService:  ClientesService
+    private clientesService:  MockClientesService
+  ){}
+ 
   ngOnInit() {
+    this.GetClientesService();
   }
+ 
+  GetFamiliasArticulos() {
+    this.articulosFamiliasService.get()
+    .subscribe((res:ArticuloFamilia[]) => {
+      this.Items = res;
+    });
+  }
+
 
 }
---
+++ src/app/models/cliente.ts
@@ -0,0 +1,205 @@
+export class Cliente {
+  
+  IdCliente: number;
+  Nombre: string;
+  NumeroDocumento: number;
+  TieneTrabajo: boolean;
+};
+export const Clientes: Cliente[] = [
+  {
+    IdCliente: 1, 
+    Nombre: "SANCHEZ, JUAN CARLOS" ,
+    NumeroDocumento: 7975858, 
+    TieneTrabajo:false
+  },
+  
+{
+  IdCliente: 2,
+  Nombre :"ROBLEDO, LUIS ARMANDO",
+  NumeroDocumento :7630283,
+  TieneTrabajo:false
+  },
+
+{
+  IdCliente: 3,
+  Nombre:"SOSA, SIMONA",
+  NumeroDocumento :5008308,
+  TieneTrabajo :false
+  },
+
+{
+  IdCliente: 4,
+  Nombre :"BRITO, MIGUEL F", 
+  NumeroDocumento :6515292,
+  TieneTrabajo: false
+  },
+
+{
+  IdCliente: 5,
+  Nombre: "GONZALEZ, RAMON ELADIO",
+  NumeroDocumento:8009620, 
+  TieneTrabajo: false
+  },
+
+{IdCliente: 6,
+Nombre: "ECHENIQUE, EFRAIN",
+NumeroDocumento: 8000911,
+TieneTrabajo :false
+},
+
+ {
+   IdCliente: 7,
+   Nombre: "VALDEZ, VICENTA",
+   NumeroDocumento :5478664,
+   TieneTrabajo :false
+   },
+
+ {
+   IdCliente: 8, 
+   Nombre:"ALBORNOZ, SILVIA BEATRIZ",
+   NumeroDocumento :5409899,
+   TieneTrabajo :false
+   },
+
+ {IdCliente: 9,
+  Nombre:"SOSA, CLEOFO MERCEDES",
+  NumeroDocumento: 6391998,
+  TieneTrabajo :false
+  },
+
+  {
+    IdCliente: 10, 
+    Nombre:"MARTINEZ, JULIA AZUCENA",
+    NumeroDocumento :2034920, 
+    TieneTrabajo :false
+    },
+
+  {
+    IdCliente: 11,
+    Nombre: "FERNANDEZ, ANGELA YOLANDA",
+    NumeroDocumento :4820828,
+    TieneTrabajo :false
+    },
+
+   {
+     IdCliente : 12,
+     Nombre : "ALIENDRO, OLGA",
+     NumeroDocumento :3584975,
+     TieneTrabajo: false
+     },
+  {
+    IdCliente : 13,
+    Nombre: "AMERI, OLGA ESTHER", 
+    NumeroDocumento : 5260192,
+    TieneTrabajo :false
+    },
+
+ { 
+   IdCliente : 14,
+   Nombre: "OVIEDO, AIDEE FELIPA",
+   NumeroDocumento :4294540,
+   TieneTrabajo :false
+   },
+
+  {
+     IdCliente: 15,
+     Nombre :"MORENO, ELISA CRISTINA",
+     NumeroDocumento :5738247,
+     TieneTrabajo :false
+     },
+
+ {
+   IdCliente: 16,
+   Nombre: "GOMEZ, CANDELARIA ALICIA",
+   NumeroDocumento :4108997,
+   TieneTrabajo :false
+   },
+
+  {
+    IdCliente: 17,
+    Nombre :"ORTIZ, EMILIA V",
+    NumeroDocumento :4669640,
+    TieneTrabajo :false
+    },
+
+  {
+    IdCliente : 18,
+    Nombre :"ROJAS, JORGE CORNELIO", 
+    NumeroDocumento :6550075,
+    TieneTrabajo:false
+    },
+
+   {
+     IdCliente: 19,
+     Nombre: "BUENO, GERARDO", 
+     NumeroDocumento :6544163,
+     TieneTrabajo :false
+     },
+
+    {
+      IdCliente: 20,
+      Nombre: "STREET, SUSANA MARGARITA",
+      NumeroDocumento :4889565,
+      TieneTrabajo :false
+      },
+ {
+   IdCliente: 21,
+   Nombre: "SANCHEZ, GRISELDA ELINA",
+   NumeroDocumento :26373820, 
+   TieneTrabajo :false
+   },
+
+{
+  IdCliente : 22,
+  Nombre:"CENA, MARGARITA ROSA",
+  NumeroDocumento :9954494, 
+  TieneTrabajo :false
+  },
+ {
+   IdCliente : 23,
+   Nombre: "CRISTALDO MARTINEZ, DEONILDA", 
+   NumeroDocumento :9282440, 
+   TieneTrabajo :false
+ },
+
+ {
+   IdCliente: 24,
+   Nombre: "PETRELLI, FABIANA", 
+   NumeroDocumento :1627506,
+   TieneTrabajo :false
+   },
+
+ {
+   IdCliente: 25,
+   Nombre: "MARTINEZ, GINES",
+   NumeroDocumento :6377090,
+   TieneTrabajo:false
+   },
+    
+  {
+    IdCliente: 26,
+    Nombre: "BARRIOS, AGUSTIN", 
+    NumeroDocumento :6654288,
+    TieneTrabajo :false
+    },
+
+    {
+      IdCliente: 27, 
+      Nombre: "RIVERO, SUSANA ELENA",
+      NumeroDocumento :5918437,
+      TieneTrabajo :false
+      },
+   {
+     IdCliente: 28,
+     Nombre :"BLETRAMO, LEANDRO EMANUEL", 
+     NumeroDocumento: 34273940, 
+     TieneTrabajo :false
+     },
+
+  {
+    IdCliente: 29, 
+    Nombre:"VINCENZINI, NORMA IRIS",
+    NumeroDocumento :5955981,
+    TieneTrabajo :false
+    }
+];
